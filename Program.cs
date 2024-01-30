767d2897ee32fc56b412c56eae9539bc8c676ca8
Mudassar Ali <mudassar.ali@vortx.com>
2024-01-30 22:52:55 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+
+class Program
+{
+    static void Main()
+    {
+        // Load assembly dynamically
+        Assembly assembly = Assembly.LoadFrom("ExampleAssembly.dll");
+
+        // Iterate through types in the assembly
+        foreach(Type type in assembly.GetTypes())
+        {
+            Console.WriteLine($"Type: {type.FullName}");
+
+            // Create an instance of the type
+            object instance = Activator.CreateInstance(type);
+
+            // Invoke a method dynamically
+            MethodInfo method = type.GetMethod("ExampleMethod");
+            method.Invoke(instance, null);
+
+            // Access properties dynamically
+            PropertyInfo property = type.GetProperty("ExampleProperty");
+            property.SetValue(instance, "New Value");
+            Console.WriteLine($"Property Value: {property.GetValue(instance)}");
+        }
+    }
+}
